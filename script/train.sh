5411d5c9c66e6a02b27ea85648d2478fabd9c5fc
Yves Greatti <ygrepo@gmail.com>
2025-09-10 17:19:55 -0400

    Added training

--- script/train.sh
+++ script/train.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 export nnUNet_raw="$PWD/data/nnUNet_raw"
 export nnUNet_preprocessed="$PWD/data/nnUNet_preprocessed"
 export nnUNet_results="$PWD/data/nnUNet_results"
+export nnUNet_compile=False
+
 # 3D full-res (all folds)
 for f in 0 1 2 3 4; do
   nnUNetv2_train 501 3d_fullres $f -p nnUNetResEncUNetXLPlans
