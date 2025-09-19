4952e44678ed5d52f829a5e65a5ebdda75ed0018
Yves Greatti <ygrepo@gmail.com>
2025-09-19 18:17:03 -0400

    Added test job

--- src/test_job.py
+++ src/test_job.py
@@ -1,1 +1,6 @@
-print("Hello from Slurm and Python!")
+import nibabel as nib
+
+img = nib.load("output/preprocessed/71899681/71899681_T1_CE_3D_AX_ALIGNED.nii.gz")
+print("Shape:", img.shape)
+print("Voxel spacing:", img.header.get_zooms())
+print("Orientation:", nib.aff2axcodes(img.affine))
