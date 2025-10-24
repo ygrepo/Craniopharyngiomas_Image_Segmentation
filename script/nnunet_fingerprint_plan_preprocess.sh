8cf2c06f31c1ec8b3cf08150966c6afd651dd49e
Yves Greatti <ygrepo@gmail.com>
2025-10-24 09:23:00 -0400

    Updating script

--- script/nnunet_fingerprint_plan_preprocess.sh
+++ script/nnunet_fingerprint_plan_preprocess.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# Predict 3D masks with Fingerprint and Plan nnU-Net v2 data
-#
+#   nnunet_fingerprint_plan_preprocess.sh    —  Convert CP to nnU-Net format.
+#SBATCH --job-name=nnunet_convert_CP_data
+#SBATCH --output=logs/nnunet_convert_CP_data_%A_%a.out
+#SBATCH --error=logs/nnunet_convert_CP_data_%A_%a.err
+#SBATCH --time=04:00:00
+#SBATCH --partition=cpu
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+
+
 set -euo pipefail
 
 module purge
@@ -17,6 +26,7 @@ source script/set_unet_path.sh
 
 #nnUNetv2_extract_fingerprint -d 502 --clean
 #nnUNetv2_plan_and_preprocess -d 502 --verify_dataset_integrity --clean
-nnUNetv2_plan_and_preprocess -d 502 -pl nnUNetPlannerResEncM
+#nnUNetv2_plan_and_preprocess -d 502 -pl nnUNetPlannerResEncM
+nnUNetv2_plan_and_preprocess -d 503 --verify_dataset_integrity --clean -pl nnUNetPlannerResEncM -np 8
 
 echo "[ok]"
