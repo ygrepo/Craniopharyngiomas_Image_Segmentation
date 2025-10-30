4b0e03c6423823a96d3c91f116dc643834a6261e
Yves Greatti <ygrepo@gmail.com>
2025-10-30 15:07:15 -0400

    Collecting validation metrics

--- script/nnunet_compute_validation_metrics.sh
+++ script/nnunet_compute_validation_metrics.sh
@@ -21,7 +21,7 @@ DATASET_ID=503
 DATASET_NAME=CP
 FOLD=0
 CFG=3d_fullres
-TR=nnUNetTrainerTopk10Loss
+TR=EmaDiceEarlyStopTrainer
 PLANS_ID=nnUNetResEncUNetMPlans
 
 # -------------------------------------------
