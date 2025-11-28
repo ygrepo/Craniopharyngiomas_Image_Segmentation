8767f2112b71f74ea3e118706ccff1a6600fceb9
Yves Greatti <ygrepo@gmail.com>
2025-11-28 08:58:42 -0500

    CV

--- script/tune_multinomial_lasso_hyperparameters.sh
+++ script/tune_multinomial_lasso_hyperparameters.sh
@@ -40,7 +40,7 @@ PYTHON="${ENV_PREFIX}/bin/python"
 MAIN="src/tune_multinomial_lasso_hyperparams.py"
 
 
-MODEL_TYPE="postop"
+MODEL_TYPE="preop"
 DATA_DIR="data/CP"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 LOG_FILE="${LOG_DIR}/${MODEL_TYPE}_tune_multinomial_lasso_hyperparams_${TIMESTAMP}.log"
